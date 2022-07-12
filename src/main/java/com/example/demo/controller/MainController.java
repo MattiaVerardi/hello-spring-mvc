b0293b94944b4ff207e3f58835eded1d49faa53c
MattiaVerardi <verardi.mattia97@gmail.com>
2022-07-12 16:50:42 +0200

    consegna bonus

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +14,11 @@ public class MainController {
 	@GetMapping("/home")
 	public String saluta() {
 		return "home";
+	}
+
+	@GetMapping("/ora")
+	public String ldt(Model m) {
+		m.addAttribute("Ora", LocalDateTime.now());
+		return "ora";
 	}
 }
